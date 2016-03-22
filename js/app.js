e8adb1217fa5a9a9dc39433e1fdb4fb0dccf7db4
Jonathan Reinink <jonathan@reinink.ca>
2016-03-22 18:41:22 -0400

    Install Laravel Elixir for asset compilation.

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(function() {
+
+    // Configure responsive videos
+    $('#wysiwyg').fitVids({
+        customSelector: "iframe[src*='ustream.tv'], iframe[src*='livestream.com']"
+    });
+
+    // Automatically line up menu and content columns
+    setInterval(function()
+    {
+        $('#content').css(
+        {
+            'min-height': ($('#header').height()) + 'px'
+        });
+
+    }, 100);
+
+    // Setup mobile menu
+    $('#info .menu').click(function(event)
+    {
+        // Prevent default browser behavior
+        event.preventDefault();
+
+        // Cache menu selector
+        var menu = $('#menu');
+
+        // Show/hide menu on click
+        if (menu.is(':visible'))
+        {
+            menu.removeClass('show');
+        }
+        else
+        {
+            menu.addClass('show').css(
+            {
+                height: $('body').height()
+            });
+        }
+    });
+});
